67496c55896a2aa6282dd9ce92a46121ead8f427
Gzh <1750203684@qq.com>
2021-08-14 18:19:29 +0800

    Demo

---
+++ playaudiotest/src/main/java/com/example/playaudiotest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.playaudiotest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play:
+                break;
+            case R.id.pause:
+                break;
+            case R.id.stop:
+                break;
+
+        }
+    }
+}
